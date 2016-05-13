cec0228bb246b325ffbe88f25024690e81336be2
Orion751 <Orion751@users.noreply.github.com>
2016-05-13 02:52:23 -0400

    Updated pseudocode

--- pdf2png.sh
+++ pdf2png.sh
@@ -7,16 +7,16 @@ make newDirectory
 
 for pdf in pdfDirectory
 
-    baseName=pdf (remove the last 4 characters)
+    baseName=basename "$pdf" (remove the last 4 characters)
 
-    pdfImagesDir="$newdirectory/baseName"
+    pdfImagesDir="$newdirectory/$baseName"
 
     mkdir "$pdfImagesDir"
 
-    pdfimages "$pdfDirectory/$pdf" "$pdfImagesDir/$baseName"
+    pdfimages "$pdf" "$pdfImagesDir/$baseName"
 
     for ppmPath in "$pdfImagesDir"
-        convert "$ppmPath" "$ppmPath":s/pm$/ng/
+        convert "$ppmPath" `echo "$ppmPath" | sed 's/pm$/ng/'`
 
         rm "$ppmPath"
 '
