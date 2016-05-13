d0bdf01ea169d614b7c1902e0eb755f83dd67b5d
Orion751 <Orion751@users.noreply.github.com>
2016-05-13 00:55:15 -0400

    Using multi-line comments

--- pdf2png.sh
+++ pdf2png.sh
@@ -2,10 +2,11 @@
 
 # syntax: pdf2pngzip <pdfdirectory> <newdirectory>
 
-# make newDirectory
-#
-# for pdf in pdfDirectory
-#
+: '
+make newDirectory
+
+for pdf in pdfDirectory
+'
 
 pdfDirectory="$1"
 newDirectory="$2"
